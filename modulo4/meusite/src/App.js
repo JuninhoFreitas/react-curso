e61824f30f0fb92522d60e7178d53dd85246394a
Juninho Freitas <joaofbjunior01@gmail.com>
2022-10-01 00:10:09 -0300

    part 1 completa

--- modulo4/meusite/src/App.js
+++ modulo4/meusite/src/App.js
@@ -3,13 +3,36 @@ class App extends Component{
    constructor(props){
     super(props);
     this.state={
-
+        email:'j@j.com',
+        senha:'',
+        sexo: 'Masculino'
     };
+    this.trocaValue = this.trocaValue.bind(this)
+   }
+   trocaValue(e){
+    console.log(e)
+        let valorDigitado = e.target.value;
+        this.setState({[e.target.name]:valorDigitado});
+        console.log(this.state)
    }
     render(){
         return(
             <div>
-                <h1>Meu Projeto</h1>
+                Email:
+                <input type="email" name="email" value={this.state.email} 
+                onChange={this.trocaValue}
+                /><br/>
+                Senha:
+                <input type="password" name="senha" value={this.state.senha} 
+                onChange={this.trocaValue}
+                />
+                <br/>
+
+                <select name="sexo" value={this.state.sexo} onChange={this.trocaValue}>
+                    <option value="masculino">Masculino</option>
+                    <option value="feminino">Feminino</option>
+                </select>
+                
             </div>
         )
     }
