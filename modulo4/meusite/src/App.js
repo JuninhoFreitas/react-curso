1e0f668feee7cde0856bd6ceee2f0c288e68104f
Juninho Freitas <joaofbjunior01@gmail.com>
2022-09-30 23:45:14 -0300

    added condit

--- modulo4/meusite/src/App.js
+++ modulo4/meusite/src/App.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
-import Membro from './components/Membro'
 class App extends Component{
-   
+   constructor(props){
+    super(props)
+    this.state = {
+        status:true
+    };
+    this.sair = this.sair.bind(this)
+    this.entrar = this.entrar.bind(this)
+   }
+   entrar() {
+    this.setState({status:true});
+   }    
+   sair() {
+    this.setState({status:false});
+   }    
     render(){
         return(
             <div>
-                <Membro nome="visitante" />
+                
+                {
+                    this.state.status? <div><h1>Entrou</h1><button onClick={this.sair}>sair</button></div> : <div><h1>deslogado  <h1/></h1><button onClick={this.entrar}>entrar</button></div>
+                }
             </div>
         )
     }
