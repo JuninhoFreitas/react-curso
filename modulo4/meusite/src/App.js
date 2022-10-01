35b7e3c5467f9dfeef63d2b1bff610b2bb22ae58
Juninho Freitas <joaofbjunior01@gmail.com>
2022-09-30 23:51:04 -0300

    added lists

--- modulo4/meusite/src/App.js
+++ modulo4/meusite/src/App.js
@@ -1,15 +1,30 @@
 import React, {Component} from 'react';
+
+const Lista = (props)=>{
+    return(
+        <div>
+            <h1>{props.name}</h1>
+            <h2>{props.age}</h2>
+        </div>
+    )
+}
 class App extends Component{
    constructor(props){
     super(props);
     this.state={
-
+        listaDePessoas:[
+            {id:1, name: 'Juninho', age:20},
+            {id:2, name: 'Elieser', age:20},
+            {id:3, name: 'Oscar', age:25},
+        ]
     };
    }
     render(){
         return(
             <div>
-                <h1>Meu Projeto</h1>
+                {this.state.listaDePessoas.map((pessoa)=>{
+                    return(<Lista key={pessoa.id} name={pessoa.name} age={pessoa.age}/>)
+                })}
             </div>
         )
     }
