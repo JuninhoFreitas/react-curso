a1a619a79e9e88533e432f658383b3ae9b6e7b79
Juninho Freitas <joaofbjunior01@gmail.com>
2022-10-01 00:52:40 -0300

    done

--- modulo4/meusite/src/App.js
+++ modulo4/meusite/src/App.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
+//https://sujeitoprogramador.com/rn-api/?api=posts
 class App extends Component{
    constructor(props){
     super(props);
     this.state={
-
+        json:[]
     };
+   }
+   componentDidMount(){
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+    fetch(url).then(
+        (r)=>r.json())
+        .then((json)=>{
+            console.log(json)
+            this.setState({json: json});
+        })
    }
     render(){
         return(
             <div>
-                <h1>Meu Projeto</h1>
+              {this.state.json.map((item)=>{
+                    return(
+
+                        <article key={item.id}>
+                            {item.titulo}
+                            <img src={item.capa}></img>
+                            </article>
+                        
+                    )
+                })}
             </div>
         )
     }
