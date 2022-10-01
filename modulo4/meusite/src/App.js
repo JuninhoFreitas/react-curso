d653d4a5fdd0584535fb1bb4aa0b2d6c31a5eea4
Juninho Freitas <joaofbjunior01@gmail.com>
2022-10-01 00:38:02 -0300

    done

--- modulo4/meusite/src/App.js
+++ modulo4/meusite/src/App.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
+import './estilo.css'
 class App extends Component{
    constructor(props){
     super(props);
     this.state={
-
+        textoFrase:'',
     };
-   }
+    this.quebraBiscoito = this.quebraBiscoito.bind(this)
+    this.frases = ['Frase1', 'frase2', 'frase3']
+}
+quebraBiscoito(){
+    const randomInt = Math.floor(Math.random() * this.frases.length);
+    console.log(randomInt)
+    this.setState({textoFrase:this.frases[randomInt]});
+}
+    render(){
+        return(
+            <div className="container">
+                <img src={require('./assets/biscoito.png')} className="img"/>
+                <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
+                <h3 className="textoFrase">{this.state.textoFrase}</h3>
+            </div>
+        )
+    }
+}
+
+class Botao extends Component{
     render(){
         return(
             <div>
-                <h1>Meu Projeto</h1>
+                <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
             </div>
         )
     }
